074c9150778897804b6242c3708174555c852765
Richard Wang <rw22448@gmail.com>
2020-11-21 19:13:11 +1100

    Remove unused import

--- src/Components/Profile/ProfileHeader/AccountInfo/ProfileIconResolver.styles.tsx
+++ src/Components/Profile/ProfileHeader/AccountInfo/ProfileIconResolver.styles.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { AlertCircle } from 'react-feather';
 import { Theme } from '../../../Styles/Themes/Themes';
 
 interface ProfileIconBackgroundProps {
