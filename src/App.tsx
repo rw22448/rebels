2bd19ee8811561748f5816f84f6ceeaf632b88df
Richard Wang <rw22448@gmail.com>
2020-12-25 20:58:12 +1100

    Disallow auto refetching on window focus for axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { Main } from './Components/Main/Main';
 import { Profile } from './Components/Profile/Profile';
@@ -10,27 +11,37 @@ import { ThemeContext } from './Components/Contexts/ThemeContext';
 const App = () => {
   const { theme } = useContext(ThemeContext);
 
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyle />
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <>
+            <GlobalStyle />
 
-          <div className="App">
-            <BrowserRouter>
-              <Switch>
-                <Route path="/" exact component={Main} />
-                <Route
-                  path="/profile/:region/:username"
-                  exact
-                  component={Profile}
-                />
-                <Route path="/" render={() => <div>404</div>} />
-              </Switch>
-            </BrowserRouter>
-          </div>
-        </>
-      </ThemeProvider>
+            <div className="App">
+              <BrowserRouter>
+                <Switch>
+                  <Route path="/" exact component={Main} />
+                  <Route
+                    path="/profile/:region/:summonerName"
+                    exact
+                    component={Profile}
+                  />
+                  <Route path="/" render={() => <div>404</div>} />
+                </Switch>
+              </BrowserRouter>
+            </div>
+          </>
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 };
