55bfc5a4afb9c7bc46390650ed973e323eae370b
Richard Wang <rw22448@gmail.com>
2021-07-13 18:00:31 +1000

    Begin structuring ChampionSquare components

---
+++ src/Components/Profile/MatchHistoryModule/MatchChampions/ChampionSquare/ChampionSquare.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { UnitsDTO } from '../../../MatchDTO';
+
+interface ChampionSquareProps {
+  champion: UnitsDTO;
+}
+
+const ChampionSquare = ({ champion }: ChampionSquareProps) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export { ChampionSquare };
--- src/Components/Profile/MatchHistoryModule/MatchChampions/MatchChampions.tsx
+++ src/Components/Profile/MatchHistoryModule/MatchChampions/MatchChampions.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { UnitsDTO } from '../../MatchDTO';
+import { ChampionSquare } from './ChampionSquare/ChampionSquare';
 
 interface MatchChampionsProps {
   championList: UnitsDTO[];
 }
 
 const MatchChampions = ({ championList }: MatchChampionsProps) => {
-  return <></>;
+  return (
+    <>
+      {championList.map((champion, index) => (
+        <ChampionSquare champion={champion} key={index} />
+      ))}
+    </>
+  );
 };
 
 export { MatchChampions };
